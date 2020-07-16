d2dd32570dfc99a3d272044f4abe2fbd19ec041c
Bradley-Krigbaum <bradley.krigbaum@gmail.com>
2020-07-15 20:59:15 -0700

    adding file updates to repo

--- src/adv.py
+++ src/adv.py
@@ -40,7 +40,8 @@ room['treasure'].s_to = room['narrow']
 # Make a new player object that is currently in the 'outside' room.
 
 player_char = {
-    'location': Room("Outside")
+    'name': 'Brad',
+    'room': 'outside'
 }
 
 # Write a loop that:
@@ -53,3 +54,87 @@ player_char = {
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+
+current_room = player_char['room']
+selected_room = ''
+
+
+while True:
+    # if player_char['name'].lower() == 'brad':
+    #     player_char['name'] = input("\nWhat is your name adventurer?\n>  ")
+
+    # if player_char['name'].lower() == '':
+    #     player_char['name'] = 'nobody'.upper()
+
+    if player_char['room'] == 'outside':
+        current_room = player_char['room']
+        room_description = room[current_room].__str__()
+        print(F"\nHello, Adventurer.\nYou are currently {room[current_room]}")
+        player_input = input("\n\nwhere to go?\npress N, N, N or... N?\n>  ")
+
+        if player_input.lower() == 'n':
+            selected_room = room[current_room].n_to
+            current_room = 'foyer'
+            player_char['room'] = current_room
+            # print(current_room)
+            print(F"\n\n\n\n\n    You entered the {selected_room}\n\n\n\n")
+            new_input = input("Walk N, S, or E?\n>  ")
+
+        elif player_input.lower() == 'q' or 'quit' or 'exit':
+            break
+        else:
+            print("\n\nThat movement isn't allowed!\nPlease input a real direction!")
+
+    elif player_char['room'] == 'foyer':
+        if new_input.lower() == 's':
+            selected_room = room[current_room].s_to
+            current_room = 'outside'
+            player_char['room'] = current_room
+            print(F"\n\n\n\n\n\n\n\n    You walked South.\n{selected_room}\n\n\n\n\n\n\n\n")
+
+        elif new_input.lower() == 'n':
+            selected_room = room[current_room].n_to
+            current_room = 'overlook'
+            player_char['room'] = current_room
+            print(F"\n\n\n\n\n\n\n\n    You walked North.\n{selected_room}\n\n\n\n\n\n\n\n")
+
+        elif new_input.lower() == 'e':
+            selected_room = room[current_room].e_to
+            current_room = 'narrow'
+            player_char['room'] = current_room
+            print(F"\n\n\n\n\n\n\n\n    You walked Ease.\n{selected_room}\n\n\n\n\n\n\n\n")
+
+        elif new_input.lower() == 'q' or 'quit' or 'exit':
+            break
+        else:
+            print("\n\nThat movement isn't allowed!\nPlease input a real direction!")
+
+    elif player_char['room'] == 'overlook':
+        print(player_char['room'])
+        break
+        # elif new_input.lower() == 'q' or 'quit' or 'exit':
+        #     break
+        # else:
+        #     print("\n\nThat movement isn't allowed!\nPlease input a real direction!")
+
+    elif player_char['room'] == 'narrow':
+        print(player_char['room'])
+        break
+        # elif new_input.lower() == 'q' or 'quit' or 'exit':
+        #     break
+        # else:
+        #     print("\n\nThat movement isn't allowed!\nPlease input a real direction!")
+
+    elif player_char['room'] == 'treasure':
+        print(player_char['room'])
+        break
+        # elif new_input.lower() == 'q' or 'quit' or 'exit':
+        #     break
+        # else:
+        #     print("\n\nThat movement isn't allowed!\nPlease input a real direction!")
+
+    elif player_input.lower() == 'q' or 'quit' or 'exit':
+        break
+    else:
+        print("\n\nThat movement isn't allowed!\nPlease input a real direction!")
